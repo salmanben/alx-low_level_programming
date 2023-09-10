8d06ddff44e587b3bc92319b3cda749c1f94d787
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-09-10 16:25:48 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,42 +11,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node;
-    
-    if (key == NULL || strlen(key) == 0 || value == NULL)
-    {
-        fprintf(stderr, "Key and value must not be empty or NULL.\n");
-        return (0);
-    }
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-    index = hash_djb2((const unsigned char *)key) % ht->size;
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return (0);
-    }
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        fprintf(stderr, "Key duplication failed.\n");
-        free(new_node);
-        return (0);
-    }
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        fprintf(stderr, "Value duplication failed.\n");
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-    return (1);
+	return (1);
 }
