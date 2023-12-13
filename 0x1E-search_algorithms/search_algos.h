ad475c7747339272a41df1131f1356e8b34c8595
salmanben <salmanbenomar250@gmail.com>
2023-12-13 10:02:30 -0800

    Fix error in header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,10 @@
-#ifndef SEARCH_ALGOS_h
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 #include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-#define SEARCH_ALGOS_h
+
+#endif /* SEARCH_ALGOS_H */
+
