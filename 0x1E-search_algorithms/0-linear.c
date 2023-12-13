53a044e615e6d9fe1c5961897c0f0afe8915f574
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-12-13 19:06:16 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,18 +10,19 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL)
-        return (-1);
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%zu] = [%d]\n",i , array[i]);
-        if (array[i] == value)
-        {
-            return (i);
-            break;
-        }
-    }
-    return (-1);
+	for (i = 0; (i < size) && (array); i++)
+	{
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+		}
+	}
+	return (-1);
 }
