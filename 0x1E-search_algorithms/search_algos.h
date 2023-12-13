01ef688d1fa4838ff13dbd2e3a99f6f70e83fea2
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-12-13 19:14:44 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,8 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_search_index(int *array, size_t l, size_t r, int value);
+void print_array(int *array, size_t left, size_t right);
 
 #endif /* SEARCH_ALGOS_H */
 
